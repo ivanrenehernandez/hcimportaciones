758dcd1b0e9f4262478b51eeafbcff2624d14f71
ivanhernandez <ivanrenehc@ufps.edu.co>
2020-10-26 22:35:45 -0500

    Avances

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
-define("base_url", "http://localhost/proyectos/HCImportaciones/");
+// define("base_url", "http://localhost/proyectos/HCImportaciones/");
+define("base_url", "https://limitless-cove-20027.herokuapp.com/");
 define("controller_default", "HomeController");
 define("action_default", "index");
-
