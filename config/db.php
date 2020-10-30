5ee8f1d7d18fdc95db7ea6b44376a4236bd27a7f
ivanhernandez <ivanrenehc@ufps.edu.co>
2020-10-30 16:11:10 -0500

    avances para heroku parametros 2

--- config/db.php
+++ config/db.php
@@ -1,9 +1,12 @@
 <?php
-class Conexion{
-	public static function connect(){
-		$db = new mysqli('localhost:3308', 'root', '', 'tienda_master');
+class Conexion
+{
+	public static function connect()
+	{
+		$db = new mysqli('us-cdbr-east-02.cleardb.com:3306', 'b687065cd3ef28', '6d4fe31e', 'heroku_839e95a79aa9f67');
+		// $db = new mysqli('localhost:3308', 'root', '', 'tienda_master');
 		$db->query("SET NAMES 'utf8'");
 		return $db;
 	}
 }
-
+// mysql://b687065cd3ef28:6d4fe31e@us-cdbr-east-02.cleardb.com/heroku_839e95a79aa9f67?reconnect=true
